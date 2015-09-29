3742974ee35ef1134c684474153c6b4728d8b891
mc <philipp@crate.io>
2015-09-29 21:58:18 +0200

    collect stars!

--- test.py
+++ test.py
@@ -35,74 +35,57 @@ class Game(object):
 
         window.game.add.sprite(0, 0, 'sky')
 
-        self.player = window.game.add.sprite(32, window.game.world.height - 150, 'dude')
-        window.game.physics.arcade.enable(self.player)
-
-        #  Player physics properties. Give the little guy a slight bounce.
-        self.player.body.bounce.y = 0.2
-        self.player.body.gravity.y = 6
-        self.player.body.collideWorldBounds = True
-
         self.platforms = window.game.add.group()
-        ground = self.platforms.create(0, window.game.world.height - 64, 'ground')
+        self.platforms.enableBody = True
 
-        window.game.physics.arcade.enable(ground)
+        ground = self.platforms.create(0, window.game.world.height - 64, 'ground')
         ground.scale.setTo(2, 2)
         ground.body.immovable = True
 
-        #  Our two animations, walking left and right.
+        ledge = self.platforms.create(400, 400, 'ground')
+        ledge.body.immovable = True
+
+        ledge = self.platforms.create(-150, 200, 'ground')
+        ledge.body.immovable = True
+
+        self.player = window.game.add.sprite(32, window.game.world.height - 150, 'dude')
+        window.game.physics.arcade.enable(self.player)
+
+        self.player.body.bounce.y = 0.2
+        self.player.body.gravity.y = 300
+        self.player.body.collideWorldBounds = True
+
         self.player.animations.add('left', [0, 1, 2, 3], 10, True)
         self.player.animations.add('right', [5, 6, 7, 8], 10, True)
 
-        #  Finally some self.stars to collect
-        self.stars = window.game.add.group()
-
-        #  Here we'll create 12 of them evenly spaced apart
-        for i in range(0, 12):
-            #  Create a star inside of the 'self.stars' group
-            star = self.stars.create(i * 70, 0, 'star')
-            window.game.physics.arcade.enable(star)
-
-            #  Let gravity do its thing
-            star.body.gravity.y = 6
-
-            #  This just gives each star a slightly random bounce value
-            star.body.bounce.y = 0.7 + random.random() * 0.2
-
-        #  The score
-        self.scoreText = window.game.add.text(16, 16, 'score: 0', {"fontSize": '32px', "fill": '#000'})
-
-        #  Our controls.
         self.cursors = window.game.input.keyboard.createCursorKeys()
 
+        self.stars = window.game.add.group()
+        self.stars.enableBody = True
+        for i in range(0, 12):
+            star = self.stars.create(i * 70, 150, 'star')
+            star.body.gravity.y = 6
+            star.body.bounce.y = 0.7 + random.random() * 0.2
+
     def collectStar(self, player, star):
-        window.console.log('collect star')
-        star.kill()
-        player.kill()
-        self.score += 10
-        self.scoreText.content = 'Score: {}'.format(self.score)
+        self.stars.remove(star)
 
     def update(self):
-        window.console.log('game-update')
         window.game.physics.arcade.collide(self.player, self.platforms)
         window.game.physics.arcade.collide(self.stars, self.platforms)
-        #  Checks to see if the self.player overlaps with any of the self.stars, if he does call the collectStar function
-        window.game.physics.arcade.overlap(self.player, self.stars, self.collectStar, None, None)
-        #  Reset the self.players velocity (movement)
+        window.game.physics.arcade.overlap(self.player, self.stars, self.collectStar, None, self)
+
         self.player.body.velocity.x = 0
         if self.cursors.left.isDown:
-            #  Move to the left
-            self.player.body.velocity.x = -150
+            self.player.body.velocity.x = - 150
             self.player.animations.play('left')
         elif self.cursors.right.isDown:
-            #  Move to the right
             self.player.body.velocity.x = 150
             self.player.animations.play('right')
         else:
-            #  Stand still
             self.player.animations.stop()
             self.player.frame = 4
-        #  Allow the self.player to jump if they are touching the ground.
+
         if self.cursors.up.isDown and self.player.body.touching.down:
             self.player.body.velocity.y = -350
 
